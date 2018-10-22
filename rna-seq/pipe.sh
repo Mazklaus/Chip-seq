98b47338c3470ab83e95cbb4e35925e7852a8835
Mazklaus <garnier.abel@orange.fr>
2018-10-22 17:39:27 +0200

    make a clean version of the help of the function

--- rna-seq/pipe.sh
+++ rna-seq/pipe.sh
@@ -8,11 +8,11 @@
 # core to use (more = faster)
 # It will be better if i put a conditional here allowing to do both
 # relative or absolute path to $1 $2
+# add an option to allow users to get a fastqc output giving a quality control view
 
 #Definition of the options
 #: means an option is required
-TEMP=`getopt -o AGNpis:kr:v:S:h --long AT,GC,dupliPos,dupliID,subsetSNP:,keepIntermediate,randomIndividuals:,splitIndiv:,randomSNP:,help -n 'qualityControl.sh' -- "$@"`
-TEMP=`getopt -o hi:f:p:s:c: --long help,index,fastq,paired,snps,core -n 'pipe.sh' -- "$@"`
+TEMP=`getopt -o hp:s:c: --long help,paired,snps,core -n 'pipe.sh' -- "$@"`
 eval set -- "$TEMP"
 
 #Initialise the flags that will be modified by the different options.
@@ -24,25 +24,30 @@ argCore=1
 
 # function for help
 usage() {
-	clear
+	#clear
 
 	echo -e "#######################################################################\n"
-	echo -e "pipe.sh is a shell script taking as input a fileset of genomic data, namely SNP genotypes in the standard Plink format (BED/BIM/FAM). Adding different options allow subsetting of your initial dataset, however relying on the Plink software for most of the operations. Operations can be inputted simultaneously.\n"
-	echo -e "\e[1m./pipe.sh [OPTION(S)] [INDEX FILE]\e[0m\n"
-	echo -e "\t\e[1m-A/--AT\e[0m\t\t\t\t\tRemoves any SNP for which the alleles are A/T or T/A.\n\t\t\t\t\t\tThese alleles are troublesome because there may be ambiguities in the strands.\n"
-	echo -e "\t\e[1m-G/--GC\e[0m\t\t\t\t\tRemoves any SNP for which the alleles are G/C or C/G for the same reasons as above.\n"
-	echo -e "\t\e[1m-N\e[0m\t\t\t\t\tRemoves any SNP for which one of the two alleles is N.\n\t\t\t\t\t\tThe joker nucleotide 'N' can be found when quality of typing is low and most of the alleles\n\t\t\t\t\t\tin individuals were not clearly defined.\n"
-	echo -e "\t\e[1m-p/--dupliPos\e[0m\t\t\t\tRemoves every SNP sharing the same position.\n\t\t\t\t\t\t\e[31m/!\ \e[0m Do not add this option if you allow multiallelic SNP.\n"
-	echo -e "\t\e[1m-i/--dupliID\e[0m\t\t\t\tRemoves every SNP sharing their rsID with one or more SNP.\n"
-	echo -e "\t\e[1m-s/--subsetSNP [.bim FILE]\e[0m\t\tSubset the SNP of your fileset by the list of SNP of another fileset.\n"
-	echo -e "\t\e[1m-r/--randomIndividuals [INTEGER]\e[0m\tRandomly pick a defined number of individuals and generates the corresponding subset.\n"
-	echo -e "\t\e[1m-v/--splitIndividuals\e[0m\t\t\tRandomly picks a defined number of individuals and generates the corresponding dataset.\n\t\t\t\t\t\tAdditionally provides a dataset with the remaining individuals of the original dataset.\n"
-	echo -e "\t\e[1m-S/--randomSNP\e[0m\t\t\t\tRandomly picks a defined number of SNP.\n"
-	echo -e "\t\e[1m-k/--keepIntermediate\e[0m\t\t\tSince operations can be simultaneous (but are always executed in the same order),\n\t\t\t\t\t\tyou can choose to keep the datasets at each step.\n\t\t\t\t\t\t\e[31m/!\ \e[0m This can flood your folders with numerous and potentially heavy files.\n"
+	echo -e "\e[1mNAME\e[0m\n"
+	echo -e "pipe.sh is a shell pipeline based on the hisat2 package which allow to easely align reads from\nRNA-seq experiment to a reference genome. It's final goal is to cover the entire analysis.\n"
+	echo -e "\e[1mDESCRIPTION\e[0m\n"
+	echo -e "\t\e[1m./pipe.sh [OPTION(S)] [INDEX FILE] [FASTQ file]\e[0m\n"
+	echo -e "\t\e[1m[INDEX FILE]\e[0m\t\t\tThe fasta file that will be used to build the index.\n"
+	echo -e "\t\e[1m[FASTQ file]\e[0m\t\t\tThe fastq file containing the reads you want to align.\n"
+	echo -e "\e[1mOPTION\e[0m\n"
+	echo -e "\t\e[1m-h|--help\e[0m\t\t\tPrint the help.\n"
+	echo -e "\t\e[1m-p|--paired [.fastq FILE]\e[0m\tUsed this argument with the paired file if you have pair-end\n\t\t\t\t\tdata (without it asume that you are managing single end data).\n"
+	echo -e "\t\e[1m-s|--snps [.fa FILE]\e[0m\t\tGive a file adding snps information to the index.\n"
+	echo -e "\t\e[1m-c/--core [INTEGER]\e[0m\t\tNumber of CPU core used for alignement.\n\t\t\t\t\t\e[31m/!\ \e[0m A high number can improve speed but can also reduce\n\t\t\t\t\toverall computer speed during the computing time.\n"
 	echo -e "\n#######################################################################"
 
 	exit 1
 }
+
+# print help if no argument
+if [[ "$@" = "--" ]]; then
+  usage
+	exit 1
+fi
 
 
 # Parsing input parameters
@@ -53,34 +58,6 @@ do
 	case "$1" in
 		-h|help)
 			usage ; shift ;;
-		-i|index)
-			case "$2" in
-				"")
-					echo "You need to give the fasta file used for index the genome of the studied species" ;
-          exit 1;;
-				*)
-					if [ -e $2 ]; then
-						argIndex=$2
-					else
-						echo "The fasta file for the index does not exist !";
-						exit 1;
-					fi;
-					shift 2 ;;
-			esac ;;
-		-f|fastq)
-			case "$2" in
-				"")
-          echo "You need to give the fastaq file you want to align" ;
-          exit 1;;
-				*)
-          if [ -e $2 ]; then
-            argFastq=$2
-          else
-            echo "The fastaq file you are trying to align does not exist !";
-            exit 1;
-          fi;
-          shift 2 ;;
-			esac ;;
 		-p|paired)
 			case "$2" in
 				"")
@@ -132,7 +109,7 @@ mkdir ./aligmnent_res
 # Production of the index file
 if [ -n "$argPaired" ]; then
   hisat2-build --snp ./$argSnps ./$argIndex ./built_index/refer
-elif
+else
   hisat2-build ./$argIndex ./built_index/refer
 fi
 
