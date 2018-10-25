03f9d5b1a4d2c7f4ba78f72cfdb84de4c3324bf5
Mazklaus <garnier.abel@orange.fr>
2018-10-25 11:17:39 +0200

    adding a quality control step and a normalisation step to the pipeline

--- microArray/analysis.R
+++ microArray/analysis.R
@@ -5,6 +5,7 @@
 
 ##### Packages installation #####
 
+install.packages("hexbin")
 source("https://bioconductor.org/biocLite.R")
 biocLite("affy")
 biocLite("limma")
@@ -13,11 +14,25 @@ biocLite("limma")
 
 library(affy)
 library(limma)
+library(hexbin)
 
 ##### Reading CEL files #####
 
 #maybe try to make a script usable in command line ans thus make pathToData as a parameter
-pathToData <- "~/../../mnt/c/Users/team5.DESKTOP-GQ2GHLO/Desktop/travail_abel/projet/dataMining_IL22BP/RNA-seq_data/GSE99802"
+pathToData <- "~/Desktop/microArrayExperiment/GSE99802/"
+setwd(pathToData) ## We are obligated to go to the directory since ReadAffy seems not able to work outside of it
 
-celFileNames <- list.celfiles(pathToData)
-data <- ReadAffy(filenames = celFileNames, celfile.path = pathToData,compress = TRUE)
+celFileNames <- list.celfiles()
+data <- ReadAffy(filenames = celFileNames,compress = TRUE)
+
+##### Quality control #####
+
+data.expr <- exprs(data)
+plot(hexplom(log2(data.expr))) #skeep this step during test since it last a very long time (using almost all the memory of the pc)
+
+##### Normalisation #####
+
+data.rma <- rma(data)
+
+##### LIMMA analysis #####
+
