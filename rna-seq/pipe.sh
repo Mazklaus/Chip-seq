0b61ca8c0ee2c46feb250237f2147094e20956e8
Mazklaus <garnier.abel@orange.fr>
2018-10-25 17:07:59 +0200

    The paired parameters work now I add a new file needed in order to get the new counts table the help is up to date with new description

--- rna-seq/pipe.sh
+++ rna-seq/pipe.sh
@@ -1,19 +1,16 @@
-/#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # This a aligmnent test pipeline in order to manage RNA-seq data
 # This script need to be adapt for both single and pair-end
 # it will need to let the people choose
 # add an option to add snp file for index building
-# the option for p in hisat2 correspond to the number of
-# core to use (more = faster)
 # It will be better if i put a conditional here allowing to do both
 # relative or absolute path to $1 $2
-# add an option to allow users to get a fastqc output giving a quality control view
 # need to add the possibility to choose the output directory
 
 #Definition of the options
 #: means an option is required
-TEMP=`getopt -o hp:s:c: --long help,paired,snps,core -n 'pipe.sh' -- "$@"`
+TEMP=`getopt -o hqp:s:c: --long help,quality,paired,snps,core -n 'pipe.sh' -- "$@"`
 eval set -- "$TEMP"
 
 #Initialise the flags that will be modified by the different options.
@@ -32,11 +29,13 @@ usage() {
 	echo -e "\e[1mNAME\e[0m\n"
 	echo -e "pipe.sh is a shell pipeline based on the hisat2 package which allow to easely align reads from\nRNA-seq experiment to a reference genome. It's final goal is to cover the entire analysis.\n"
 	echo -e "\e[1mDESCRIPTION\e[0m\n"
-	echo -e "\t\e[1m./pipe.sh [OPTION(S)] [INDEX FILE] [FASTQ file]\e[0m\n"
+	echo -e "\t\e[1m./pipe.sh [OPTION(S)] [INDEX FILE] [FASTQ FILE] [ANNOTATION FILE]\e[0m\n"
 	echo -e "\t\e[1m[INDEX FILE]\e[0m\t\t\tThe fasta file that will be used to build the index.\n"
-	echo -e "\t\e[1m[FASTQ file]\e[0m\t\t\tThe fastq file containing the reads you want to align.\n"
+	echo -e "\t\e[1m[FASTQ FILE]\e[0m\t\t\tThe fastq file containing the reads you want to align.\n"
+	echo -e "\t\e[1m[ANNOTATION FILE]\e[0m\t\tThe gtf file containing the position of genes for your\n\t\t\t\t\treferecence genome(WORK IN PROGRESS not really needed).\n"
 	echo -e "\e[1mOPTION\e[0m\n"
 	echo -e "\t\e[1m-h|--help\e[0m\t\t\tPrint the help.\n"
+	echo -e "\t\e[1m-q|--quality\e[0m\t\t\tGive a html quality summary using fastqc.\n"
 	echo -e "\t\e[1m-p|--paired [.fastq FILE]\e[0m\tUsed this argument with the paired file if you have pair-end\n\t\t\t\t\tdata (without it asume that you are managing single end data).\n"
 	echo -e "\t\e[1m-s|--snps [.fa FILE]\e[0m\t\tGive a file adding snps information to the index.\n"
 	echo -e "\t\e[1m-c/--core [INTEGER]\e[0m\t\tNumber of CPU core used for alignement.\n\t\t\t\t\t\e[31m/!\ \e[0m A high number can improve speed but can also reduce\n\t\t\t\t\toverall computer speed during the computing time.\n"
@@ -60,6 +59,8 @@ do
 	case "$1" in
 		-h|help)
 			usage ; shift ;;
+		-q|quality)
+			argQualityControl=1 ; shift ;;
 		-p|paired)
 			case "$2" in
 				"")
@@ -107,15 +108,24 @@ done
 # Creation of the output folder
 mkdir ./built_index
 mkdir ./aligmnent_res
-mkdir ./quality_control
+if [ $argQualityControl = 1 ];then
+	mkdir ./quality_control
+fi
 
 # Production of the index file
 if [ -n "$argPaired" ]; then
-  hisat2-build -p $argCore --snp ./$argSnps ./$1 ./built_index/refer
+	if [ -n "$argSnps" ]; then
+	  hisat2-build -p $argCore --snp ./$argSnps ./$1 ./built_index/refer
+	else
+	  hisat2-build -p $argCore ./$1 ./built_index/refer
+	fi
 else
-  hisat2-build -p $argCore ./$1 ./built_index/refer
+	if [ -n "$argSnps" ]; then
+		hisat2-build -p $argCore --snp ./$argSnps -1 ./$1 -2 $argSnps ./built_index/refer
+	else
+		hisat2-build -p $argCore -1 ./$1 -2 $argSnps ./built_index/refer
+	fi
 fi
-
 # Alignement of the file
 hisat2 -p $argCore -x ./built_index/refer -U ./$2 -S ./aligmnent_res/aligmnent.sam
 
@@ -126,3 +136,7 @@ samtools view -bS ./aligmnent_res/aligmnent.sam > ./aligmnent_res/alignment.bam
 if [ $argQualityControl = 1 ]; then
 	fastqc -o ./quality_control ./aligmnent_res/alignment.bam
 fi
+
+# Generation of the count table
+# the -s parameters must be put on yes if the alignement is strand specific
+htseq-count -f bam -r pos -s no ./aligmnent_res/alignment.bam $3 > ./aligmnent_res/samples.counts
